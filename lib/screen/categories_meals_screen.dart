45cb609169019458dbee0849d9901521a7811f08
Bene-Tesla <benevaniosantos930@gmail.com>
2024-01-15 21:37:28 -0300

    :tada: criando o componente de rotas

--- lib/screen/categories_meals_screen.dart
+++ lib/screen/categories_meals_screen.dart
@@ -4,16 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:food/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  final Category category;
-  const CategoriesMealsScreen(this.category);
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)!.settings.arguments as Category;
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text(category.title)),
       ),
-      body: const Center(
-        child: Text('Receitas por Categoria'),
+      body: Center(
+        child: Text('Receitas por Categoria ${category.id}'),
       ),
     );
   }
